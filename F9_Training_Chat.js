1ee75636f818109a2069ef7c9093022edd683bcb
GCCBHeffernan <123589716+GCCBHeffernan@users.noreply.github.com>
2023-01-26 16:46:50 +0000

    Update F9_Training_Chat.js

--- F9_Training_Chat.js
+++ F9_Training_Chat.js
@@ -104,7 +104,7 @@ var options = {
 	"autostart": true,
 	"profileLabel": "Chat - Training",
 	"theme": "https://gccbheffernan.github.io/GCC_Chat_Training/Training_ChatStyle.css",
-	"logo": "https://gccbheffernan.github.io/GCC_Chat_Training/ECC_Mark_White.png",
+	"logo": "https://gccbheffernan.github.io/GCC_Chat_Training/ECC_Logo_White.png",
 // 	"logo": "https://banyanhill.com/wp-content/uploads/2018/12/banyan-logo-New_white.png",
 	"surveyOptions": {
 		"showComment": false,
